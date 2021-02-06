e01b4034766b8930aba745b1f5bb78ee8c44cb98
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-02-06 21:18:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
     end
     min_num = array[0]
     array.each do |value|
-      min.num = value if value < min_num
+      min_num = value if value < min_num
     end
     name_hash.each do |k, value|
       if min_num == value
